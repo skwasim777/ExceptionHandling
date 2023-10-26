4da186f5fce0049b11daec4f395c725e4ab62343
skwasim777 <wasimsk6061@gmail.com>
2023-10-26 13:17:26 +0530

    another example of handling arrayindexoutofboundexception

---
+++ src/Exceptions/Example03.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class Example03 {
+	public static void main(String[] args) {
+		try {
+			int arr[] = { 1, 2, 3 };
+			System.out.println(arr[10]);// may throw exception
+
+		} catch (ArrayIndexOutOfBoundsException e){// handling theexception
+			System.out.println(e);
+		}
+		System.out.println("rest of the code");
+	}
+}
