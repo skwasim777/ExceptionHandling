e1ab7b7f32da152aebd757d77d9e341a90e4ff8e
skwasim777 <wasimsk6061@gmail.com>
2023-10-26 12:20:33 +0530

    handling arithmetic exception

---
+++ src/Exceptions/Example01.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class Example01 {
+	public static void main(String[] args) {
+		try {
+			int data = 100 / 0;
+		} catch (ArithmeticException e) {
+
+		}
+		System.out.println("rest of the code");
+
+	}
+}
